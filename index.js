34ac45045c149a7c2576745b7e44f749a57ab6b9
heholo <heholo@live.dk>
2017-11-27 11:15:20 +0100

    color

---
+++ index.js
@@ -0,0 +1,7 @@
+d3.select(window).on('load', init);
+
+function init(){
+    d3.select('body')
+        .append('div')
+        .html("<img src=\"http://your-photography.com/files/2016/07/HH0A1943.jpg\" height=\"200\" width=\"200\">")
+}
