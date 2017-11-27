3c48d03cc2368f15dce5bd15c8ead3abc9eaad7c
heholo <heholo@live.dk>
2017-11-27 12:20:30 +0100

    Adding LoL +/-

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 d3.select(window).on('load', init);
 
 function init(){
-    d3.select('body')
-        .append('div')
+    d3.select('#script1')
         .html("<img src=\"http://your-photography.com/files/2016/07/HH0A1943.jpg\" height=\"200\" width=\"200\">")
 }
