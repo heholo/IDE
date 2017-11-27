b18794cfe4ce105dbf97c57772913197aebc3384
William Sprent <bsprent@gmail.com>
2017-11-27 20:36:48 +0100

    stuff

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 d3.select(window).on('load', init);
 
-function init(){
-    d3.select('#script1')
-        .html("<img src=\"http://your-photography.com/files/2016/07/HH0A1943.jpg\" height=\"200\" width=\"200\">")
+function addRandomCatGif() {
+    var newDiv = d3.select('#script1')
+                   .append('div')
+    newDiv.html('<a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&type=gif&timestamp=' + Date.now() + '"></a>')
+    newDiv.append('p').html('Inserted by javascript (see <a href="www.thecatapi.com">www.thecatapi.com</a>)')
+}
+
+function init() {
+    var newDiv = d3.select('#script1')
+                   .append('div')
+    newDiv.html("<img src=\"http://your-photography.com/files/2016/07/HH0A1943.jpg\">")
+    newDiv.append('p').text('Inserted by javascript')
+    setInterval(addRandomCatGif, 20000)
 }
