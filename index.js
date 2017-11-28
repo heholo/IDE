50253da5361fad65aff9dae9432746e808c47135
William Sprent <bsprent@gmail.com>
2017-11-28 21:20:28 +0100

    stop the cat invasion

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 d3.select(window).on('load', init);
 
+var cats;
+
 function addRandomCatGif() {
     var newDiv = d3.select('#script1')
                    .append('div')
@@ -7,10 +9,14 @@ function addRandomCatGif() {
     newDiv.append('p').html('Inserted by javascript (see <a href="www.thecatapi.com">www.thecatapi.com</a>)')
 }
 
+function stopCats() {
+    clearInterval(cats)
+}
+
 function init() {
     var newDiv = d3.select('#script1')
                    .append('div')
     newDiv.html("<img src=\"http://your-photography.com/files/2016/07/HH0A1943.jpg\">")
     newDiv.append('p').text('Inserted by javascript')
-    setInterval(addRandomCatGif, 5000)
+    cats = setInterval(addRandomCatGif, 5000)
 }
