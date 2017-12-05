5236c4956f7e71a63963add9cee14f0109e7164c
heholo <heholo@live.dk>
2017-12-05 14:09:26 +0100

    Added least squares function

--- week2/index.js
+++ week2/index.js
@@ -143,10 +143,30 @@ function plotTemperatureDifference(d) {
 
 }
 
-
 var parseTime = d3.timeParse('%Y');
 
+// returns slope, intercept and r-square of the line
+function leastSquares(xSeries, ySeries) {
+    var reduceSumFunc = function(prev, cur) { return prev + cur; };
 
+    var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
+    var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
+
+    var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
+        .reduce(reduceSumFunc);
+
+    var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
+        .reduce(reduceSumFunc);
+
+    var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
+        .reduce(reduceSumFunc);
+
+    var slope = ssXY / ssXX;
+    var intercept = yBar - (xBar * slope);
+    var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
+
+    return [slope, intercept, rSquare];
+}
 
 function handleData(d) {
     const parser = d3.dsvFormat(' ');
