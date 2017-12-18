f9f0519ba36c0dd5ce7c1a179cbdaacbe2175334
heholo <heholo@live.dk>
2017-12-18 11:12:53 +0100

    Weird

--- week4/index.js
+++ week4/index.js
@@ -1,1 +1,21 @@
 d3.select(window).on('load', init);
+
+var hands;
+
+var dims;
+
+function init() {
+  dims=d3.select("svg").node().getBBox(); // finding dimensions
+  d3.csv('hands.csv', function(data) {
+    hands = data // bind to global variable
+  });
+  d3.csv('hands_pca.csv', plotPCA);
+}
+
+function plotPCA(data) {
+
+}
+
+function plotHand(data) {
+
+}
