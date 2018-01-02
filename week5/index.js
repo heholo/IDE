180b650019ef3cc4f903153db4838faaa92fdd58
heholo <heholo@live.dk>
2018-01-02 15:29:27 +0100

    added week5 init

---
+++ week5/index.js
@@ -0,0 +1,16 @@
+d3.select(window).on("load", init);
+
+// global variables
+
+const margin = {
+  top: 20,
+  bottom: 30,
+  left: 10,
+  right: 30,
+}
+
+// function init
+
+function init() {
+
+}
