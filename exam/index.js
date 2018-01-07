10581e23f8f6ee74d35b07c0902f0a77222c4de1
William Sprent <williams@unity3d.com>
2018-01-07 18:37:25 +0100

    data is now utf8

--- exam/index.js
+++ exam/index.js
@@ -12,8 +12,46 @@ const margin = {
 // function init
 
 function init() {
-  $.getJSON('data/ghetto.json', function(d) {
-    console.log(d)
-  });
-  //var ghetto = JSON.parse(ghetto);
+  d3.queue()
+    .defer(d3.json, "data/ghetto.json")
+    .defer(d3.csv, "data/muni_pop.csv")
+    .defer(d3.csv, "data/muni_emp.csv")
+    .defer(d3.csv, "data/muni_foreigners.csv")
+    .defer(d3.csv, "data/muni_edu.csv")
+    .await(_init)
+}
+
+/*
+ * Create an object containing total and year by year
+ * population by muni 
+ **/
+function collect_pops(pop) {
+  var res = {}
+  var years = pop.columns.slice(2)
+
+  years.forEach()
+  pop.forEach(function(o) {
+    if (!res[o.municipality])  {
+      res[o.municipality] = {
+        agegroups: {},
+        total: 0,
+        municipality: o.municipality
+      }
+    }
+
+    // sum
+
+    res[o.municipality].total += o.
+    
+  })
+
+  return res
+}
+
+function _init(err, ghetto, pop, emp, foreigners, edu) {
+  console.log(ghetto)
+  console.log(collect_pops(pop))
+  console.log(emp)
+  console.log(foreigners)
+  console.log(edu)
 }
